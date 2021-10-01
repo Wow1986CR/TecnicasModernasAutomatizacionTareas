caebd4dfccd277b0dddcc7e2a85a527a6d9405ac
alvar <alvaro5218@gmail.com>
2021-09-30 22:29:08 -0600

    Commit Final (correccion de errores)

--- Tarea01.js
+++ Tarea01.js
@@ -1,57 +1,74 @@
-let letters = ['a', 'a', 'a', 'b', 'b', 'c', 'a', 'b', 'c'];
+//EJERCICIO 01
+/*
+Profe este no lo logre resolver. Experimente con dos funciones.
+La primera trae de vuelta el conteo correcto, pero es una funcion imperativa y devuelve un string.
+La segunda entra un poco mas dentro del paradigma de programacion funcional pero no devuelve un
+array de arrays y el conteo recorre todo el array sin importar si es consecutivo o no.
+*/
+
+let array = ['a', 'a', 'a', 'b', 'b', 'c', 'a', 'b', 'c']
+
+function cntConsecEl(array) {
+   let result = "";
+   let counter = 1;
+  
+ for (let i = 0; i < array.length; i++) {
+    if (array[i] === array[i + 1]) {
+      counter++;
+      
+    } else {
+      result += array[i] + counter;
+      counter = 1;
+    }
+  }
+   return result;
+ }
+console.log(cntConsecEl(array));
 
 
-console.log([0,1,1,2,3,4].reduce(function(valorAnterior, valorActual, indice, vector){
-    return valorAnterior + valorActual;
-  }));
-  
-  // Primera llamada
-  valorAnterior = 0, valorActual = 1, indice = 1
-  
-  // Segunda llamada
-  valorAnterior = 1, valorActual = 2, indice = 2
-  
-  // Tercera llamada
-  valorAnterior = 3, valorActual = 3, indice = 3
-  
-  // Cuarta llamada
-  valorAnterior = 6, valorActual = 4, indice = 4
-  
-  // el array sobre el que se llama a reduce siempre es el objeto [0,1,2,3,4]
-  
-  // Valor Devuelto: 10
 
-const arr = ['a','b','c','d','d','e','a','b','c','f','g','h','h','h','e','a'];
+const arr = ['a', 'a', 'a', 'b', 'b', 'c', 'a', 'b', 'c'];
 
-// const validador = (previousV, currentV) => {
-//     previousV[currentV] = (previousV[currentV] || 0) + 1;
-//   return previousV;
-// }
-// const map = arr.reduce(validador, []);
-// console.log(map);
-// console.log(arr);
+const validador = (previousV, currentV) => {
+    previousV[currentV] = (previousV[currentV] || 0) + 1;
+  return previousV;
+}
+const map = arr.reduce(validador, []);
+console.log(map);
+
 
 //EJERCICIO 02
 let arr1 = ['a', ['b', 2], 3, null, [[4], ['c']]];
-function flattenDeep(arr1) {
-   return arr1.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val), []);
+
+function flatten(arr1) {
+   return arr1.reduce((acc, val) => Array.isArray(val) ? acc.concat(flatten(val)) : acc.concat(val), []);
 }
-console.log(flattenDeep(arr1));
+console.log(flatten(arr1));
 
 //EJERCICIO 03
-let array3 = ['super','bow','bowl','tar','get','book','let'];
+/*
+Profe este ejercicio no logre resolverlo segun lo indicado. Tiene un resultado booleano.
+*/
 
-
-let word = 'superbowl';
-console.log(Array.from(word));
-
-
-function wordMatcher(array3,word) {
-
+function match(array, word){
+  var char_array = word.split(''),
+      char, index;
+  array = array.join('').split('');
+  while(char_array.length > 0){
+      char = char_array.shift();
+      if ((index = array.indexOf(char)) > -1)
+          array.splice(index, 1);
+      else
+          return false;
+  }
+  return true;
 }
 
+console.log(match(['super','bow','bowl','tar','get','book','let'], "superbowl"))
+
+
 //EJERCICIO 04
-function squareNumber(num){
+function fn(num){
     let numbString = num + "";
     let total = "";
     for(let i in numbString){
@@ -60,4 +77,4 @@ function squareNumber(num){
     return parseInt(total);
   }
 
-console.log(squareNumber(123));
+console.log(fn(123));
